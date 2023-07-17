89deaaea2b617b64e5e2a8c35c96fc90548877c0
Dayo Banjo <dayobanjo231@gmail.com>
2023-07-17 02:21:45 -0700

    Change landing page

--- src/data.js
+++ src/data.js
@@ -48,13 +48,37 @@ const projectData = [
 
 export const WritingData = [
     {
+        title: "Flower Bloom Animation with SwiftUI",
+        link: "https://banjodayo39.medium.com/flower-bloom-animation-with-swiftui-bfb99a02e901"
+    },
+    {
+        title: "SwiftUI Metal Shaders",
+        link: "https://banjodayo39.medium.com/swiftui-metal-shaders-c53e7c690cb5"
+    },
+    {
+        title: "ABC of Augmented Reality 3D Data Visualization",
+        link: "https://banjodayo39.medium.com/abc-of-augmented-reality-3d-data-visualization-reality-composer-ar-quicklook-f3c7712eb529"
+    },
+    {
+        title: "Custom Workout Alert with Dynamic Island and Live Activities",
+        link: "https://banjodayo39.medium.com/customworkout-alert-with-dynamic-island-and-live-activities-ios-16-tutorial-iphone-14-6b29ca1a87a1"
+    },
+    {
         title: "Understanding Language Modeling for Dummies",
         link: "https://medium.com/@banjodayo39/understanding-language-modeling-for-dummies-47ba7aaa9455"
     },
     {
         title: "NLP: Naive Bayes vs Logistic Regression",
         link: "https://medium.com/@banjodayo39/nlp-text-classification-with-naive-bayes-vs-logistic-regression-7ad428d4cafa"
-    }
+    },
+    {
+        title: "How Pixels are Formed",
+        link: "https://banjodayo39.medium.com/how-pixels-are-formed-c644a35af6ed"
+    },
+    {
+        title: "Implementation of Text Generation with GRU",
+        link: "https://banjodayo39.medium.com/implementation-of-text-generation-with-gated-recurrent-unit-deep-learning-ac11c4347af2"
+    },
 ]
-
+//SwiftUI Metal Shaders
 export default projectData;
